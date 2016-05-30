89376876c8bebe18b8e2d1168d3fce8d684ad248
Muhammad Iqbal <bali2u@msn.com>
2016-05-30 14:06:41 +0500

    initial commit - SDK usage with swift

---
+++ SDKStarterSwift/ObjCHeader.h
@@ -0,0 +1,14 @@
+//
+//  ObjCHeader.h
+//  SDKStarterSwift
+//
+//  Created by Muhammad Iqbal on 30/05/2016.
+//  Copyright © 2016 Sentiance Corporation. All rights reserved.
+//
+
+#ifndef ObjCHeader_h
+#define ObjCHeader_h
+
+#import <SENTTransportDetectionSDK/SENTTransportDetectionSDK.h>
+
+#endif /* ObjCHeader_h */
